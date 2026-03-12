d25f5d155fbda19a7f68e94ba51408069f4116d3
Uli Rizki <ulirizki@gmail.com>
2026-03-12 10:16:27 +0700

    first endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/mahasiswa', function(){
+    $data = [
+        'nim' => "001",
+        'nama' => "Uli"
+    ];
+
+    return response()->json($data);
+});
