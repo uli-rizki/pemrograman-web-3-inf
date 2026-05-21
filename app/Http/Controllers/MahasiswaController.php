1e76eb0db645bc87a9d19704855c9ba311842882
uli lab <ulirizki@gmail.com>
2026-05-21 12:07:43 +0700

    update mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -60,4 +60,51 @@ class MahasiswaController extends Controller
             ], 422);
         }
     }
+
+    // menambhkan fungsi add mahasiswa
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        // validasi form
+        $validator = Validator::make($request->all(), [
+            'nim' => 'required',
+            'nama_lengkap' => 'required',
+            'kelas' => 'required'
+        ], [
+            'nim.unique' => 'Nim sudah ada'
+        ]);
+
+        // cek jika ada eror validasi form
+        if($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'error' => $validator->errors()
+            ], 422);
+        }
+
+        $mahasiswa = Mahasiswa::find($id);
+        if(! $mahasiswa) {
+             return response()->json([
+                'status' => 'error',
+                'error' => 'Data mahasiswa tidak ditemukan'
+            ], 422);
+        }
+
+        // menyimpan data
+        $mahasiswa->fill($request->all());
+        $simpan = $mahasiswa->save();
+
+        if($simpan) {
+            return response()->json([
+                'status' => 'success'
+            ], 201);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'error' => 'Gagal menyimpan data'
+            ], 422);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
     Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+    Route::post('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
 
     Route::get('/matakuliah', [MatakuliahController::class, 'index']);
 
